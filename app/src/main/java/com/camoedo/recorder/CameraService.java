dfc18b92b49952ed964a9d0d1674a2a34748c2ae
Claudio Amoedo <claudioamoedo@gmail.com>
2015-09-22 21:16:25 -0300

    Add service notification

--- app/src/main/java/com/camoedo/recorder/CameraService.java
+++ app/src/main/java/com/camoedo/recorder/CameraService.java
@@ -1,10 +1,20 @@
 package com.camoedo.recorder;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
 public class CameraService extends Service {
+
+    private static final String TAG = "CameraService";
+    private static final int NOTIFICATION_ID = 1;
+
+    NotificationManager notificationManager;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -14,6 +24,43 @@ public class CameraService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Notification.Builder mBuilder =
+                new Notification.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        Notification notification = mBuilder.build();
+        notification.flags = notification.flags | Notification.FLAG_ONGOING_EVENT;
+
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        notificationManager.cancel(NOTIFICATION_ID);
     }
 
     @Override
